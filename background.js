c530027d7f55a4046575973f1b113441b69f29a4
Gregory Wint <gregwint95@gmail.com>
2019-02-09 19:54:58 -0500

    Changed background.js and manifest files so that extension is operational on gmail page

--- background.js
+++ background.js
@@ -10,17 +10,16 @@ chrome.runtime.onInstalled.addListener(function() {
 	chrome.storage.sync.set({color: color_blue}, function() {
 		console.log("The color is blau.");
 	});
-	
+
 	// adds declared rules to the background script
 	chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
 		chrome.declarativeContent.onPageChanged.addRules([{
 			conditions : [new chrome.declarativeContent.PageStateMatcher({
-				pageUrl : {hostEquals: 'developer.chrome.com'},
+				pageUrl : {hostEquals: 'mail.google.com'},
 			})],
 				actions: [new chrome.declarativeContent.ShowPageAction()]
 		}]);
-	});	
-
-}); 
+	});
+});
 
 console.log("What's up?!?!?!? My first Chrome extension!!");
