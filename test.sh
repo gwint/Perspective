1285db5f88f74bc8d998ec2f33beefbd897917fe
Gregory Wint <gregwint95@gmail.com>
2019-08-11 17:45:39 -0400

    Remove unused files

--- test.sh
+++
@@ -1,5 +0,0 @@
-curl -X POST -u "apikey:t6dFCXw5GdOrTrCS8sM7iTaED0GCJmS93ksh-6VLJFyu" \
---header "Content-Type: application/json" \
---data-binary @tone.json \
-"https://gateway.watsonplatform.net/tone-analyzer/api/v3/tone?version=2017-09-21"
-
